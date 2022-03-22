51ae4b5e60d25f15cea077a968ccc6868aad1331
IvanGame <46857116+IvanGame@users.noreply.github.com>
2022-03-22 12:20:21 +0700

    Add files via upload
    Add back-to-top button

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ header__burger.onclick = function () {
     back.classList.toggle('lock');
 }
 
-header__list.onclick = function () {
-    header__list.classList.remove('active');
-    back.classList.toggle('lock');
-}
+$('.back-to-top').click(function () {
+    $('body,html').animate({ scrollTop: 0 }, 800); // 800 - Скорость анимации
+});
+
+$(window).scroll(function () { // Отслеживаем начало прокрутки
+    let scrolled = $(window).scrollTop(); // Вычисляем сколько было прокручено.
+
+    if (scrolled > 350) { // Если высота прокрутки больше 350 - показываем кнопку
+        $('.back-to-top').addClass('active');
+    } else {
+        $('.back-to-top').removeClass('active');
+    }
+});
